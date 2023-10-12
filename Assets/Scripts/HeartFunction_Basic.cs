7a26dff9e0c0910637a8d354f7ede538cdafe879
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-10-12 16:28:48 +0800

    update

---
+++ Assets/Scripts/HeartFunction_Basic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace liou
+{
+
+    public class HeartFunction_Basic : MonoBehaviour
+    {
+        [SerializeField] private int HealAmountt = 1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
+
